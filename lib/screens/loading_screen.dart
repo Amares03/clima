d4d5d94c6c8b0f8ef42f8a293cbb38570ac6aeac
Amares03 <amaressabit0314@gmail.com>
2022-03-15 21:59:42 +0300

    refactore the loading file to network related files

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,25 +1,40 @@
 // ignore_for_file: use_key_in_widget_constructors, deprecated_member_use, prefer_const_constructors
 
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 
 //weather example url
 //http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=029966da0b30bcfe8a338e4e12c35819
+
+const apiKey = '029966da0b30bcfe8a338e4e12c35819';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double? latitude;
+  double? longtude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
+
+    latitude = location.latitude;
+    longtude = location.longtude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longtude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
   }
 
   @override
